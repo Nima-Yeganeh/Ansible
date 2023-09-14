a0b61202118e9eca83a15e453ba2df108f5cbc22
root <yeganeh.nima.d@gmail.com>
2023-09-14 11:42:48 +0330

    done

--- consul/test2/consul_info_check.sh
+++ consul/test2/consul_info_check.sh
@@ -1,2 +1,2 @@
-ansible-playbook -i ansible_hosts.ini consul_info_check.yml | more
+ansible-playbook -i ansible_hosts.ini consul_info_check.yml
 # ansible-playbook -i ansible_hosts.ini consul_info_check.yml -K
