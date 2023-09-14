9e5ac9138832a1ef293667c00f0a12304830930f
root <yeganeh.nima.d@gmail.com>
2023-09-14 20:05:58 +0330

    done

---
+++ mongodb/test1/commands.sh
@@ -0,0 +1,6 @@
+
+bash mongodb_deploy.sh
+
+bash mongodb_rs_status_check.sh
+ansible-playbook -i ansible_hosts.ini mongodb_rs_status_check.yml
+
---
+++ mongodb/test1/mongodb_backup_on_secondary.sh
@@ -0,0 +1,2 @@
+
+ansible-playbook -i ansible_hosts.ini mongodb_backup_on_secondary.yml -K
